8d737db9ce4f7abca4c6f704eb2d5e76910fac69
Antsa Rafanomezantsoa <217477665+aignosia@users.noreply.github.com>
2025-12-22 13:14:55 +0300

    Add search and autocomplete

--- back/app/books/services.py
+++ back/app/books/services.py
@@ -1,7 +1,9 @@
 from fastapi import HTTPException
-from sqlmodel import Session, select
+from sqlmodel import Session, func, or_, select
 
+from app.authors.models import Author
 from app.books.models import Book, BookCreate
+from app.links.models import BookAuthorLink
 
 
 def add_book(book: BookCreate, session: Session):
@@ -22,3 +24,59 @@ def get_book(book_id: int, session: Session):
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
+
+
+def get_books_search(q: str, session: Session):
+    query_term = " & ".join(f"{word}" for word in q.split())
+    ts_query = func.to_tsquery("simple", query_term)
+
+    book_vector = func.to_tsvector("simple", func.coalesce(Book.title, ""))
+    author_vector = func.to_tsvector(
+        "simple",
+        func.coalesce(Author.firstname, "")
+        + " "
+        + func.coalesce(Author.lastname),
+    )
+
+    results = session.exec(
+        select(Book)
+        .outerjoin(BookAuthorLink)
+        .outerjoin(Author)
+        .where(
+            or_(
+                book_vector.op("@@")(ts_query),
+                author_vector.op("@@")(ts_query),
+            )
+        )
+        .distinct()
+    ).all()
+
+    return results
+
+
+def get_search_autocomplete(q: str, session: Session):
+    query_term = " & ".join(f"{word}:*" for word in q.split())
+    ts_query = func.to_tsquery("simple", query_term)
+
+    book_tsvector = func.to_tsvector("simple", func.coalesce(Book.title, ""))
+    author_tsvector = func.to_tsvector(
+        "simple",
+        func.coalesce(Author.firstname, "")
+        + " "
+        + func.coalesce(Author.lastname, ""),
+    )
+
+    results = list(
+        session.exec(
+            select(Book.title).where(book_tsvector.op("@@")(ts_query))
+        ).all()
+    ) + list(
+        " ".join(it)
+        for it in session.exec(
+            select(Author.firstname, Author.lastname).where(
+                author_tsvector.op("@@")(ts_query)
+            )
+        ).all()
+    )
+
+    return results[:10]
